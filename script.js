1b6bb577d14861fe95663a340abbb944f38bd8d3
nobugsatcoding <nobugsatcoding@gmail.com>
2023-07-19 12:04:46 +0300

    added JS countdown page, random equations generator

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const penaltyTimeEl = document.querySelector('.penalty-time');
 const playAgainBtn = document.querySelector('.play-again');
 
 // Equations
-
+let questionAmount = 0;
 let equationsArray = [];
 
 // Game Page
@@ -32,34 +32,43 @@ const wrongFormat = [];
 
 // Scroll
 
+// Get Random Number up to a max number
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 // Create Correct/Incorrect Random Equations
 function createEquations() {
   // Randomly choose how many correct equations there should be
-  // const correctEquations = 
+  const correctEquations = getRandomInt(questionAmount);
+  console.log('correct', correctEquations);
   // Set amount of wrong equations
-  // const wrongEquations = 
+  const wrongEquations = questionAmount - correctEquations;
+  console.log('wrong', wrongEquations);
   // Loop through, multiply random numbers up to 9, push to array
-  // for (let i = 0; i < correctEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
-  //   const equationValue = firstNumber * secondNumber;
-  //   const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
-  //   equationObject = { value: equation, evaluated: 'true' };
-  //   equationsArray.push(equationObject);
-  // }
+  for (let i = 0; i < correctEquations; i++) {
+    firstNumber = getRandomInt(9);
+    secondNumber = getRandomInt(9);
+    const equationValue = firstNumber * secondNumber;
+    const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
+    equationObject = { value: equation, evaluated: 'true' };
+    equationsArray.push(equationObject);
+  }
   // Loop through, mess with the equation results, push to array
-  // for (let i = 0; i < wrongEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
-  //   const equationValue = firstNumber * secondNumber;
-  //   wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
-  //   wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
-  //   wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
-  //   const formatChoice = 
-  //   const equation = wrongFormat[formatChoice];
-  //   equationObject = { value: equation, evaluated: 'false' };
-  //   equationsArray.push(equationObject);
-  // }
+  for (let i = 0; i < wrongEquations; i++) {
+    firstNumber = getRandomInt(9);
+    secondNumber = getRandomInt(9);
+    const equationValue = firstNumber * secondNumber;
+    wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
+    wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
+    wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
+    const formatChoice = getRandomInt(3);
+    const equation = wrongFormat[formatChoice];
+    equationObject = { value: equation, evaluated: 'false' };
+    equationsArray.push(equationObject);
+  }
+  shuffle(equationsArray);
+  console.log('equations array', equationsArray);
 }
 
 // Dynamically adding correct/incorrect equations
@@ -82,3 +91,60 @@ function createEquations() {
 //   bottomSpacer.classList.add('height-500');
 //   itemContainer.appendChild(bottomSpacer);
 // }
+
+// Displays 3, 2, 1, GO!
+function countdownStart() {
+    countdown.textContent = '3';
+    setTimeout(() => {
+        countdown.textContent = '2';
+    }, 1000);
+    setTimeout(() => {
+        countdown.textContent = '1';
+    }, 2000);
+    setTimeout(() => {
+        countdown.textContent = 'GO!';
+    }, 3000);
+}
+
+// Navigate from Splash page to countdown Page
+function showCountdown() {
+    countdownPage.hidden = false;
+    splashPage.hidden = true;
+    countdownStart();
+    createEquations();
+}
+
+// Get the value from selected radio button
+function getRadioValue() {
+    let radioValue;
+    radioInputs.forEach((radioInput) => {
+        if (radioInput.checked) {
+            radioValue = radioInput.value;
+        }
+    });
+    return radioValue;
+}
+
+// Form that decides amount of questions
+function selectQuestionAmount(e) {
+    e.preventDefault();
+    questionAmount = getRadioValue();
+    console.log('qst amount:', questionAmount);
+    if (questionAmount) {
+        showCountdown();
+    }
+}
+
+startForm.addEventListener('click', () => {
+    radioContainers.forEach((radioEl) => {
+        // Remove Selected Label Styling
+        radioEl.classList.remove('selected-label');
+        // Add it back if radio input is cheked
+        if (radioEl.children[1].checked) {
+            radioEl.classList.add('selected-label');
+        }
+    });
+});
+
+// Event Listeners
+startForm.addEventListener('submit', selectQuestionAmount);
